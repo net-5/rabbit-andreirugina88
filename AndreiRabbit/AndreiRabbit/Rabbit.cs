0ab4652005a1a98b45da02a3377aa5e5b1e78774
andreirugina88 <50754191+andreirugina88@users.noreply.github.com>
2019-07-04 16:07:54 +0300

    Update Rabbit.cs

--- AndreiRabbit/AndreiRabbit/Rabbit.cs
+++ AndreiRabbit/AndreiRabbit/Rabbit.cs
@@ -2,9 +2,113 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AndreiRabbit
+namespace TheRabbit
 {
-    class Rabbit
+    //Model the Rabbit class knowing that:
+
+    //A rabbit can have Blue, Red or Black eyes.
+    //A rabbit's fur can be white, brown, black or grey.
+    //It has a gender.
+    //It has a birth date, and based on that you should be able to see how old the rabbit is.
+    //We also know that a rabbit is a mammal that moves, sleeps and eats.
+
+    public enum Eyes
     {
+        blue,
+        red,
+        black
+    }
+
+    public enum Fur
+    {
+        white,
+        brown,
+        black,
+        grey
+    }
+
+    public enum Gender
+    {
+        male,
+        female
+    }
+    public class Rabbit
+    {
+        Eyes eyes;
+        Fur fur;
+        Gender gender;
+
+        public Eyes Eyes
+        {
+            get { return this.eyes; }
+            set { this.eyes = value; }
+        }
+
+        public Fur Fur
+        {
+            get { return this.fur; }
+            set { this.fur = value; }
+        }
+        public Gender Gender
+        {
+            get { return this.gender; }
+            set { this.gender = value; }
+        }
+        private DateTime birthDate;
+        public DateTime BirthDate
+        {
+            get { return this.birthDate; }
+            set { this.birthDate = value; }
+        }
+
+        public Rabbit(Eyes eyes, Fur fur, Gender gender, DateTime birthDate)
+        {
+            Eyes = eyes;
+            Fur = fur;
+            Gender = gender;
+            this.birthDate = birthDate;
+        }
+
+        public int Age
+        {
+            get
+            {
+                DateTime currentDate = DateTime.Today;
+                int age = currentDate.Year - BirthDate.Year;
+
+                if (currentDate.Month < BirthDate.Month || (currentDate.Month == BirthDate.Month && currentDate.Day < BirthDate.Day))
+                {
+                    age--;
+                }
+                return age;
+            }
+        }
+
+        public string MyDescription()
+        {
+            return $"I am a {Gender} rabbit. I have {Eyes} eyes. My fur is {Fur}. I am {Age} years old.";
+        }
+
+        public string MyAge()
+        {
+            return $"I am a {(Age).ToString()} years old {Gender} rabbit.";
+        }
+
+        public string Moveing()
+        {
+            return $"I am a very fast animal.";
+        }
+
+        public string Sleeping()
+        {
+            return $"I sleep a little and I am very vigilant during my sleep.";
+        }
+
+        public string Eating()
+        {
+            return $"I'm a rodent who eats carrots and cabbage.";
+        }
+
+
     }
 }
